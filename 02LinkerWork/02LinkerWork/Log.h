b2504958569f2d76c5c6c861837361051b8bdac6
ZP <991475623@qq.com>
2022-08-14 16:15:54 +0800

    updata

---
+++ 02LinkerWork/02LinkerWork/Log.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <iostream>
+
+void Log(const char* message);
+
+//inline void Log(const char* message)
+//{
+//	std::cout << message << std::endl;
+//}
---
+++ 02LinkerWork/02LinkerWork/Math.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "Log.h"
+
+//void Log(const char* message)
+//{
+//	std::cout << message << std::endl;
+//}
+
+//void Log(const char* message);
+
+int Multiply(int a, int b)
+{
+	Log("Multiply");
+	return a * b;
+}
+
+int main()
+{
+	std::cout << Multiply(5, 8) << std::endl;
+	std::cin.get();
+}
